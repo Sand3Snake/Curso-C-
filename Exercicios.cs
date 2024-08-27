87eb42c476946e118d2fabc23327ba6d01223094
Sand3Snake <163043520+Sand3Snake@users.noreply.github.com>
2024-08-27 14:09:21 -0300

    update

---
+++ Exercicios.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso_C_
+{
+   public class Advogado
+    {
+        public string nome;
+        public string oab;
+        public double salario;
+
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine($"O nome do advogado é {nome}\nSeu oab é {oab}\nSeu salário é {salario}");
+        }
+    }
+    
+ }
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using Curso_C_;
+
+Advogado advogado = new Advogado();
+advogado.oab = "asdadas";
+advogado.salario = 15.000;
+advogado.nome = "Cantoara";
+
+advogado.ExibirInformacoes();
