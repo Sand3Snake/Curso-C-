318baca88572b1b7e74cdf6c86f62b88624baa52
Sand3Snake <163043520+Sand3Snake@users.noreply.github.com>
2024-09-11 16:53:22 -0300

    update

--- obj/Debug/net8.0/Curso CSharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/Curso CSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Curso CSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0e873fad9e90cf292fadd867d55b183033d0573")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+527ea6c8fb893d521d1571754586f81ff34b138f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Curso CSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Curso CSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
